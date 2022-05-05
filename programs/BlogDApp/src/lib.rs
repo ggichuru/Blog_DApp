61f394829cad2867823714b9b5ecaebebb76d7a5
ggichuru <ggichuru.dev@gmail.com>
2022-05-05 15:57:28 +0300

    [module] Create post

--- programs/BlogDApp/src/lib.rs
+++ programs/BlogDApp/src/lib.rs
@@ -45,8 +45,39 @@ pub mod blog_d_app {
 
         Ok(())
     }
+
+    //----------------------------------------------------//
+    //                     CRUD FUNCTIONS                //
+    //--------------------------------------------------//
+    pub fn create_post(ctx: Context<CreatePost>, title: String, content: String) -> ProgramResult {
+        let blog_account = &mut ctx.accounts.blog_account;
+        let post_account = &mut ctx.accounts.post_account;
+        let user_account = &mut ctx.accounts.user_account;
+        let authority = &mut ctx.accounts.authority;
+
+        post_account.title = title;
+        post_account.content = content;
+        post_account.user = user_account.key();
+        post_account.authority = authority.key();
+        post_account.pre_post_key = blog_account.current_post_key;
+
+        // Store created post id as the current post id in the blog account
+        blog_account.current_post_key = post_account.key();
+
+        // EMIT post created event
+        emit!(PostEvent {
+            label: "CREATE".to_string(),
+            post_id: post_account.key(),
+            next_post_id: None, // Same as null
+        });
+
+        Ok(())
+    }
 }
 
+//----------------------------------------------------//
+//                     USER STRUCTS                //
+//--------------------------------------------------//
 #[derive(Accounts)]
 pub struct InitBlog<'info> {
     // Create blog_account
@@ -70,10 +101,6 @@ pub struct BlogState {
     pub current_post_key: Pubkey,
     pub authority: Pubkey,
 }
-
-// TODO: Finish up post state
-#[account]
-pub struct PostState {}
 
 #[derive(Accounts)]
 pub struct SignupUser<'info> {
@@ -99,4 +126,43 @@ pub struct UpdateUser<'info> {
     )]
     pub user_account: Account<'info, UserState>,
     pub authority: Signer<'info>,
+}
+
+//----------------------------------------------------//
+//                     CRUD STRUCTS                  //
+//--------------------------------------------------//
+#[derive(Accounts)]
+pub struct CreatePost<'info> {
+    // TODO: Check how to correctly use mut in this instance
+    #[account(init, payer = authority, space = 8 + 50 + 500 + 32 + 32 + 32)]
+    pub post_account: Account<'info, PostState>,
+
+    #[account(mut, has_one = authority)]
+    pub user_account: Account<'info, UserState>,
+
+    #[account(mut)]
+    pub blog_account: Account<'info, BlogState>,
+
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct PostState {
+    title: String,
+    content: String,
+    user: Pubkey,
+    pub pre_post_key: Pubkey,
+    pub authority: Pubkey,
+}
+
+//----------------------------------------------------//
+//                     EVENTS                        //
+//--------------------------------------------------//
+#[event]
+pub struct PostEvent {
+    pub label: String, // 'CREATE', 'UPDATE' or 'DELETE'
+    pub post_id: Pubkey,
+    pub next_post_id: Option<Pubkey>,
 }
