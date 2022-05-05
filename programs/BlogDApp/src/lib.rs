6f6d199f71f225e3b6c6fc4c1a91304857f09731
ggichuru <ggichuru.dev@gmail.com>
2022-05-05 15:38:39 +0300

    [fn] functions to handle user

--- programs/BlogDApp/src/lib.rs
+++ programs/BlogDApp/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,10 +7,96 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod blog_d_app {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    //-----------------------------------------------------//
+    //                     HANDLE USERS                   //
+    //---------------------------------------------------//
+    /*
+     * Initialize the blog account with the current_post_key and authority as blog state
+     */
+    pub fn init_blog(ctx: Context<InitBlog>) -> ProgramResult {
+        //get accounts from context
+        let blog_account = &mut ctx.accounts.blog_account;
+        let genesis_post_account = &mut ctx.accounts.genesis_post_account;
+        let authority = &mut ctx.accounts.authority;
+
+        // set the blog state
+        blog_account.authority = authority.key();
+        blog_account.current_post_key = genesis_post_account.key();
+
+        Ok(())
+    }
+
+    pub fn signup_user(ctx: Context<SignupUser>, name: String, avatar: String) -> ProgramResult {
+        let user_account = &mut ctx.accounts.user_account;
+        let authority = &mut ctx.accounts.authority;
+
+        user_account.name = name;
+        user_account.avatar = avatar;
+        user_account.authority = authority.key();
+
+        Ok(())
+    }
+
+    pub fn update_user(ctx: Context<UpdateUser>, name: String, avatar: String) -> ProgramResult {
+        let user_account = &mut ctx.accounts.user_account;
+
+        user_account.name = name;
+        user_account.avatar = avatar;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitBlog<'info> {
+    // Create blog_account
+    #[account(init, payer = authority, space = 8 + 32 + 32 + 32 + 32)]
+    pub blog_account: Account<'info, BlogState>,
+
+    // Create genisis_post_account -- initialize blog a/c with first post to create a linked list.
+    #[account(init, payer = authority, space = 8 + 32 + 32 + 32 + 32 + 8)]
+    pub genesis_post_account: Account<'info, PostState>,
+
+    // Authority is the program signer - creater of the blog - rent payer
+    pub authority: Signer<'info>,
+
+    // system program is required by runtime for creating the account
+    pub system_program: Program<'info, System>,
+}
+
+// from pseudo blog state
+#[account]
+pub struct BlogState {
+    pub current_post_key: Pubkey,
+    pub authority: Pubkey,
+}
+
+// TODO: Finish up post state
+#[account]
+pub struct PostState {}
+
+#[derive(Accounts)]
+pub struct SignupUser<'info> {
+    #[account(init, payer = authority, space = 8 + 40 + 120 + 32)]
+    pub user_account: Account<'info, UserState>,
+
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct UserState {
+    pub name: String,
+    pub avatar: String,
+    pub authority: Pubkey,
+}
+
+#[derive(Accounts)]
+pub struct UpdateUser<'info> {
+    #[account(
+        mut,
+        has_one = authority,
+    )]
+    pub user_account: Account<'info, UserState>,
+    pub authority: Signer<'info>,
+}
